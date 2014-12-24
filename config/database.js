3f65c11b26c60348907a8733b6fde5b29bf4c728
Stefano Schiavi <stefano@bvprojects.org>
2014-12-24 15:28:52 -0800

    setup mongolab db and dotenv

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
 module.exports = {
-        url : 'mongodb://localhost/glassHopper_db'
+        url : 'mongodb://'+ process.env.DB_USER +':'+ process.env.DB_PWD +'@ds049130.mongolab.com:49130/glasshopper'
     };
