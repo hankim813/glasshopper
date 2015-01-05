67656c56682162a488641f23e41109c0018841a2
Sharmila Jesupaul <tosharmila@gmail.com>
2015-01-04 19:41:29 -0800

    remove server side review time remaining

--- routes/reviews.js
+++ routes/reviews.js
@@ -22,69 +22,23 @@ router.route('/')
                               noiseLevel: req.body.noiseLevel,
                               avgAge: req.body.avgAge,
                               ggRatio: req.body.ggRatio});
-    // Review.model('review')
 
-    // var now = new Date();
-    // var timeSpan = 600; //600 seconds = 10 minutes
-    // console.log(Review.find());
-    console.log(review._author);
-    console.log(review._bar);
-    Review.findOne({_author: review._author, _bar: review._bar},
-      function(err, previousReview) {
-        console.log('previous review', previousReview);
-
-        if (previousReview) {
-          console.log('subtracted time', Math.floor((new Date() - previousReview.createdAt)/60000));
-          if(Math.floor((new Date() - previousReview.createdAt)/60000) < 10){
-            res.status(404).json(err);
-          } else {
-            review.save(function(err) {
-              if (err) {
-                res.status(400).json(err);
-              } else {
-                res.status(201).json(review);
-              }
-            });
-          }
-        } else {
-          review.save(function(err) {
-            if (err) {
-              res.status(400).json(err);
-            } else {
-              res.status(201).json(review);
-            }
-          });
-        }
+    review.save(function(err) {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(201).json(review);
+      }
     });
   });
 
 router.route('/:barId')
   .get(function(req, res) {
-    // Review.aggregate([
-    //   { $match: { _bar: req.params.barId} },
-    //   { $group: 
-    //     {
-    //       _id: "$_bar",
-    //       avgNoise: { $avg: "$noiseLevel"},
-    //       avgCrowd: { $avg: "$crowdLevel"},
-    //       avgAge:   { $avg: "$avgAge"},
-    //       avgRatio: { $avg: "$ggRatio"}
-    //     }}
-    //   ], function(err, aggregations) {
-    //     if (err)
-    //       res.send(err);
-    //     res.json(aggregations);
-    //   })
-
     Review.find({_bar: req.params.barId}, function(err, reviews) {
       if (err)
         res.send(err);
       res.json(reviews);
     });
-
-
-      // var reviews = Review.find({_bar: req.params.barId});
-      // return res.status(200).json(reviews);
   });
 // expose routes to make them available when loading this file
 module.exports = router;
